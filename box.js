7af4071d62dc53bd490b27aaf6a4c940533efd2e
ninadG2 <73230198+ninadG2@users.noreply.github.com>
2021-05-06 15:35:39 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class Box
+{
+    constructor(x, y, width, height)
+    {
+       var option ={ 
+                     'restitution':0.5,
+                   }
+        this.body = Bodies.rectangle(x, y, width, height, option)
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }   
+    
+    display(){
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill("red");
+        rect(pos.x, pos.y, this.width, this.height);
+      }
+    };
+
+
+
+
+
+
